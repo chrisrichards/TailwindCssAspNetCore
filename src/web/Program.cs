9daa7cf3ca24337ffff9bfbcb2b552281283e01a
Chris Richards <chris.richards@yellowfeather.co.uk>
2023-03-23 00:28:01 +0000

    Uses port environment variable

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -21,6 +21,16 @@ namespace TailwindCssAspNetCore.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    // For running in Railway
+                    var portVar = Environment.GetEnvironmentVariable("PORT");
+                    if (portVar is {Length: >0} && int.TryParse(portVar, out int port))
+                    {
+                        webBuilder.ConfigureKestrel(options =>
+                        {
+                            options.ListenAnyIP(port);
+                        });
+                    }
                 });
     }
 }
